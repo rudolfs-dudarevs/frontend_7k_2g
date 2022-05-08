aa35c4608c43dd313fb40708f42083c23936810b
JurisJubalts <juris.jubalts@gmail.com>
2022-05-08 19:09:49 +0300

    05: majasdarbs

---
+++ majasdarbi/05-majasdarbs/JurisJubalts/Object/cv-as-object.js
@@ -0,0 +1,47 @@
+const VardsUzvards = {
+    name: "Juris",
+    surname: "Jubalts",
+    contacts:{
+        phoneNumber: "+37129128601",
+        email: "juris.jubalts@gmail.com",
+    },
+    profession: "Pašvaldības policists",
+    workExperience:[
+        {
+            companyName: "Rīgas pašvaldības policija Latgales pārvalde",
+            fromTill: "11.2012. līdz 04.2015.",
+            position: "kārtībnieks",
+        },
+        {
+            companyName: "Rīgas pašvaldības policijas Drošības uz ūdens un civilās aizsardzības pārvalde",
+            fromTill: "04.2015. līdz 11.2016.",
+            position: "kārtībnieks-glābējs", 
+        },
+        {
+            companyName: "Rīgas pašvaldības policijas Drošības uz ūdens un civilās aizsardzības pārvalde",
+            fromTill: "12.2016. līdz 11.2019.",
+            position: "inspektors-glābējs", 
+        },
+        {
+            companyName: "Rīgas pašvaldības policijas Drošības uz ūdens un civilās aizsardzības pārvalde",
+            fromTill: "12.2019. līdz ...",
+            position: "vecākais inspektors", 
+        },
+    ],
+    languages:[
+        {
+            language: "latviešu",
+            level: "dzimtā",
+        },
+        {
+            laguage: "angļu",
+            level: "labi",
+        },
+        {
+            language: "krievu",
+            level: "labi",
+        },
+    ],
+    hobbies:["atpūta pie dabas","tehnoloģiju pasaule", "Aktīvs dzīvesveids", "Futbols"],
+    otherSkills:["B kategorijas vadītāja apliecība", "A kategorijas vadītāja apliecība","Atpūtas kuģu vadītāja apliecība"],
+};
