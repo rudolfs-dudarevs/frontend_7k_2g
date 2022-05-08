bfb81e435ba956d65bee771aef74d1e21d0f3870
eliassbaranovs <82933682+eliassbaranovs@users.noreply.github.com>
2022-05-08 15:55:29 +0300

    Last update

--- majasdarbi/03-majasdarbs/EliassBaranovs/js/app.js
+++
@@ -1,9 +0,0 @@
-function pgLoad() {
-  document.getElementById("html").classList.add("html");
-  document.getElementById("css").classList.add("css");
-  document.getElementById("js").classList.add("js");
-}
-
-window.onload = function () {
-  pgLoad();
-};
