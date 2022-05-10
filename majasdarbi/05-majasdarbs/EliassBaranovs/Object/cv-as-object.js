5ca12d5230cd41430104d25f71ec96cf972134b5
eliassbaranovs <82933682+eliassbaranovs@users.noreply.github.com>
2022-05-10 18:46:34 +0300

    Homework added

---
+++ majasdarbi/05-majasdarbs/EliassBaranovs/Object/cv-as-object.js
@@ -0,0 +1,26 @@
+const randomGuy = {
+  fullName: "Random Guy",
+  age: 30,
+  job: "Front-end developer",
+  bio: {
+    phoneNumber: "123456789",
+    location: "Riga, Latvia",
+    email: "email@email.com",
+    birthDate: "14.02.1992",
+  },
+  links: {
+    discord: "discord.gg/discord",
+    github: "github.com/github",
+    linkedin: "linkedin.com/linkedin",
+    twitter: "twitter.com/twitter",
+    facebook: "facebook.com/facebook",
+  },
+  skills: {
+    frontEnd: ["HTML", "CSS", "JavaScript"],
+    design: ["Figma", "Sketc", "Adobe PS"],
+  },
+  languages: {
+    firstLanguage: ["Latvian", "Native"],
+    secondLanguage: ["English", "Fluent"],
+  },
+};
