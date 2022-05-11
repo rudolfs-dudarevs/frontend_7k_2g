4be949684903f9e21fc3bfacd358fc22beffeae6
klibikis <vklibikis@gmail.com>
2022-05-11 21:20:09 +0300

    homework nr.5

---
+++ majasdarbi/05-majasdarbs/ValtsKlibikis/cv-as-object.js
@@ -0,0 +1,38 @@
+const person = {
+    firstName: "Valts",
+    lastName: "Klibikis",
+    profession: "soon to be frontend developer",
+    contacts: {
+        tel: +37128748221,
+        email: "vklibikis@gmail.com"
+    },
+    workExperience: {
+        company: {
+            accenture: {
+                length: "6 months",
+                role: "DevOps",
+                isStillWorking: false
+            },
+            vikAuto: {
+                length: "2 years",
+                role: "multitasking",
+                isStillWorking: true
+            },
+        },
+    },
+    education: [
+        "Proffessional bachelor’s degree in Transport electronics and telematics at Riga Technical University (RTU) 2014 – 2018",
+        "Neretas Jāņa Jaunsudrabiņa secondary school - secondary education 2002 - 2014"
+    ],
+    hobbies: [
+        "folk dancing",
+        "volleyball",
+        "board sports",
+        "carrrrrrs",
+    ],
+    languageSkills: {
+        latvian: "native",
+        english: "fluent",
+        russian: "beginner"
+    }
+}
