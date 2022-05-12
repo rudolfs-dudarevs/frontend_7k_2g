88c7c11b3b70a652da4c00e162d4c2009ef0dc0c
RobbyZirup <robertszirups@gmail.com>
2022-05-12 19:58:20 +0300

    homework-05

--- majasdarbi/05-majasdarbs/05-Roberts-Zirups/if-else.js
+++ majasdarbi/05-majasdarbs/05-Roberts-Zirups/if-else.js
@@ -1,40 +1,26 @@
-if (darbaDiena === 'Pirmdiena') {
-    console.log('1');
-} else if (darbaDiena === 'Otrdiena') {
-    console.log('2');
-} else if (darbaDiena === 'Trešdiena') {
-    console.log('3');
-} else if (darbaDiena === 'Ceturtdiena') {
-    console.log('4');
-} else if (darbaDiena === 'Piektdiena') {
-    console.log('5');
-} else if (darbaDiena === 'Sestdiena') {
-    console.log('6');
-} else if (darbaDiena === 'Svētdiena') {
-    console.log('7');
-} else {
-    console.log('Error');
-}
-
-switch (new Date().getDay()) {
-    case 1:
-        text = "Pirmdiena";
-        break;
-    case 2:
-        text = "Otrdiena";
-        break;
-    case 3:
-        text = "Trešdiena";
-        break;
-    case 4:
-        text = "Ceturdiena";
-        break;
-    case 5:
-        text = "Piektdiena";
-        break;
-    case 6:
-        text = "Sestdiena";
-        break;
-    case 7:
-        text = "Svetdiena";
-        break; 
+switch (darbaDiena) {
+    case "Pirmdiena":
+      console.log("1");
+      break;
+    case "Otrdiena":
+      console.log("2");
+      break;
+    case "Trešdiena":
+      console.log("3");
+      break;
+    case "Ceturtdiena":
+      console.log("4");
+      break;
+    case "Piektdiena":
+      console.log("5");
+      break;
+    case "Sestdiena":
+      console.log("6");
+      break;
+    case "Svētdiena":
+      console.log("7");
+      break;
+    default:
+      console.log("Error");
+      break;
+  }
--- majasdarbi/05-majasdarbs/05-Roberts-Zirups/name.js
+++ majasdarbi/05-majasdarbs/05-Roberts-Zirups/name.js
@@ -4,6 +4,7 @@ const VardsUzvards = {
     age: '34',
     email: 'robertszirups@gmail.com',
     hobbies: ['gym', 'football', 'hiking'],
+    phone: "3712888000",
     address: {
         street: 'Kapteina 87',
         city: 'Engure',
