f32670bec1cee572d79f6c5b24251628832005ab
Keitija Janevica <k.janevica@gmail.com>
2022-05-16 21:38:21 +0300

    05-majas-darbs

--- majasdarbi/05-majasdarbs/Conditionals/if-else.js
+++ majasdarbi/05-majasdarbs/Conditionals/if-else.js
@@ -1,4 +1,3 @@
-let darbaDiena = "Ceturtdiena";
 
 if (darbaDiena === 'Pirmdiena') {
     console.log('1');
@@ -16,33 +15,4 @@ if (darbaDiena === 'Pirmdiena') {
     console.log('7');
 } else {
     console.log('Error');
-}
-
-let darbaDiena = "Ceturtdiena";
-
-switch (darbaDiena) {
-
-    case 'Pirmdiena':
-        console.log("1")
-        break;
-    case 'Otrdiena':
-        console.log("2")   
-        break;
-    case 'Trešdiena':
-        console.log("3")
-        break;
-    case 'Ceturtdiena':
-        console.log("4")
-        break;
-    case 'Piektdiena':
-        console.log("5")
-        break;
-    case 'Sestdiena':
-        console.log("6")
-        break;
-    case 'Svētdiena':
-        console.log("7")   
-        break;
-    default:
-        console.log('Error')   
 }
