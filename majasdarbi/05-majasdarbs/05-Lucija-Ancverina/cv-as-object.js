499d5aecf631c9f6185b1ab35cbe8c45665e1abd
Lucija Ancverina <lucija.rozina@gmail.com>
2022-05-17 11:30:11 +0300

    5 nodarbibas majasdarbs

---
+++ majasdarbi/05-majasdarbs/05-Lucija-Ancverina/cv-as-object.js
@@ -0,0 +1,46 @@
+const VardsUzvards = {
+    firstname: "Lūcija",
+    lastname: "Ancveriņa",
+    contacts: {
+        Phone: "+37126321986",
+        Email: "lucija.rozina@gmail.com",
+    },
+    workexp: [{
+        companyName: "Jumprava Cultural Centre",
+        statuss: "CEO",
+        time: "2022-present",
+    }, {
+        companyName: "Jumprava Cultural Centre",
+        statuss: "Event Planner",
+        time: "2018-2021",
+    }, {
+        companyName: "ShowHeroes Group",
+        statuss: "Product design specialist",
+        time: "2017",
+    }],
+    Education: [{
+            School: "Ogre tehnical collage – Professional development education programm | InDesign CC in the print and media tehcnology",
+            Time: "2020",
+        },
+        {
+            School: "University of Latvia - Bacholor's degree in Classical philology",
+            Time: "2011-2014",
+        },
+        {
+            School: "Valmiera Pārgaujas state Gymnasium",
+            Time: "2008-2011",
+        },
+        {
+            School: "Valmiera Viesturs Secondary school",
+            Time: "2000-2008",
+        }
+    ],
+    Languages: [{
+        lang:"Latvian(Native)"
+    }, {
+        lang:"English(Professional Working Proficiency)"
+    }, {
+        lang: "Russian(Elementary Proficiency)"
+    }],
+Hobbies:['camping','Bass guitar','dog training','baking'],
+}
