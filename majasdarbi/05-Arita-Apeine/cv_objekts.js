87d75e7fa0fd54e7941299c8df53ca0878518898
SauleskungsVR <ivars.saule@inbox.lv>
2022-05-17 17:28:51 +0300

    5.majasdarbs

---
+++ majasdarbi/05-Arita-Apeine/cv_objekts.js
@@ -0,0 +1,41 @@
+const VardsUzvards = {
+    fullName: "Artūrs Mednis",
+	contacts: {
+		phoneNumber: "+37126545662",
+		email: "arturs@newblack.lv",
+		linkedIn: "https://www.linkedin.com/in/mednis/",
+	},
+	workExperience: [{
+		companyName: "NewBlack Agency",
+		role: "CEO",
+		fromTill: "2016 – now"
+	}, {
+		companyName: "Inspired Digital Latvia",
+		role: "Managing director",
+		fromTill: "2010 – 2016"
+	}, {
+		companyName: "Inspired Communications",
+		role: "Head of Digital",
+		fromTill: "2009 – 2010"
+	}, {
+		companyName: "Ocean Recruitment",
+		role: "Managing director"
+		fromTill: "2003 - 2009"
+	}],
+	Education: [{
+		School: "University of Latvia",
+	}, {
+		School: "Riga Secondary School No.93",
+	}]
+	Skills: [{
+		"Social media", "Advertising", "Digital marketing"
+	}]
+	LanguageSkills: [{
+		Latvian: "Native",
+		English: "Fluent",
+		Russian: "Fluent"
+	}],
+	Interests and hobbies: [{
+		"Music", "Sports", "New Technologies"
+	}]
+}
