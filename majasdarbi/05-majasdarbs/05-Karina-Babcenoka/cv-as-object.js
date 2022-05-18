aee20ef5399e5e42d209115517b37d211aa88148
Karinabcnk <babcenoka@gmail.com>
2022-05-18 22:23:14 +0300

    05: majasdarbs objects, explicit coercion and switch.

---
+++ majasdarbi/05-majasdarbs/05-Karina-Babcenoka/cv-as-object.js
@@ -0,0 +1,26 @@
+const person = {
+    firstName: "Karina",
+    lastName: "Babcenoka",
+    age: 31,
+    languages: ["Latvian", "Russian", "English"],
+    education: {
+        organisation: "Riga Technical University",
+        title: "Professional Bachelor Study in Administration of Cutoms and Taxes",
+    },
+    rolesIncompanies: {
+        EuroLiveTechnologies: {
+            role1: "Game Presenter",
+            role2: "Payroll accountant assistant",
+            role3: "Shift Administrator",
+            role4: "Team Leader of Shift Administrators",
+        },
+        Evolution: {
+            role1: "Game Presenter",
+            role2: "Planning and Scheduling Specialist",
+            role3: "Service Support Specialist",
+            role4: "Service Support Team Leader",
+        }
+    }
+};
+
+console.log(person);
