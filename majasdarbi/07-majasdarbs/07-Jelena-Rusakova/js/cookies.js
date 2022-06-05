e444362d0d2114a74c923df75ae4b9b02ac62882
Jelena Rusakova <jelerusa@gmail.com>
2022-06-06 00:41:33 +0300

    07-Jelena-Rusakova

--- majasdarbi/07-majasdarbs/07-Jelena-Rusakova/js/cookies.js
+++ majasdarbi/07-majasdarbs/07-Jelena-Rusakova/js/cookies.js
@@ -1,21 +1,20 @@
-// lai regiģētu mājaslapas cookie, piekļūstam caur document.cookie
-document.cookie = "username=Student123"; 
+
+document.cookie = "username=Student123";
 
 let cookies = document.cookie;
 console.log("Cookies:", cookies);
 
-// izveudot datuma objektu
+
 let date = new Date();
 console.log("Current date:", date);
-// iestatīt datuma objektu kā šī brīža dienu + 5 dienas
+
 date.setDate(date.getDate() + 5);
 console.log("Expiry date:", date);
 
-// iestatīt cookie derīguma termiņu
-document.cookie = `username=Student123; expires=${date}`; 
 
-// lai dzēstu cookie, jāiestate derīguma termiņš no pagājuša datuma
-// iestatīt datuma objektu kā šī brīža dienu - 10 dienas
+document.cookie = `username=Student123; expires=${date}`;
+
+
 date.setDate(date.getDate() - 10);
 console.log("Expired date:", date);
 document.cookie = `username=Student123; expires=${date}`; 
--- majasdarbi/07-majasdarbs/07-Jelena-Rusakova/js/storage.js
+++ majasdarbi/07-majasdarbs/07-Jelena-Rusakova/js/storage.js
@@ -1,6 +1,5 @@
-// local storage
+
 window.onload = () => {
-    // iegūt ierakstus no local storage pēc to nosaukuma ar .getItem() metodi
     let userName = localStorage.getItem("userName");
     let userSpeciality = localStorage.getItem("speciality");
     let userSkills = localStorage.getItem("skills");
@@ -11,8 +10,7 @@ window.onload = () => {
     let skillsOutput = document.getElementById("skills");
     let contactsOutput = document.getElementById("contacts");
 
-    // veikt pārbaudi, ja userName ieraksts ir atrasts local storage - iestatīt tā vērtību kā .innerHTML un attēlot lietotājam
-    if(userName) {
+       if(userName) {
         userNameOutput.innerHTML = `Username: ${userName}`;
     } else {
         localStorage.setItem("userName", "Student123");
@@ -26,7 +24,6 @@ window.onload = () => {
 
     if(userSkills) {
          let skillsToDisplay = [];
-         // tā kā viss local un session storage tiek saglabāts kā JSON, nepieciešams to pārvērst no JSON uz objektu
          let parsedSkills = JSON.parse(userSkills);
          console.log(parsedSkills)
 
@@ -38,9 +35,6 @@ window.onload = () => {
         skillsOutput.innerHTML = skillsToDisplay.join("");
     } else {
         let frontendSkills = ["JavaScript", "React", "CSS"];
-        // tā kā local storage viss tiek saglabāts kā string, objektus un masīvus nebūs iespējams izmantot kā objektus,
-        // kad gribēsim tos iegūt no local storage
-        // tāpēc pārvēršam šos objektus vispirms par JSON un tad saglabājam local storage
         let skillsAsJSON = JSON.stringify(frontendSkills);
         localStorage.setItem("skills", skillsAsJSON);
     }
@@ -48,15 +42,7 @@ window.onload = () => {
     if(userContacts) {
         let contactsToDisplay = [];
         let parsedContacts = JSON.parse(userContacts);
-
-        // varam iterēt cau jebkura objekta īpašību nosaukiem izmantojot Object.keys() metodi, kurā kā parametru norādām
-        // objektu, kura īpašībām vēlamies iterēt cauri
-        // šī metode atgriezīs masīvu uz kura savukārt varam izmantot .forEach() metodi
-        // kā parametrus for each iegūsim objekta īpasības nosaukumu
-        Object.keys(parsedContacts).forEach((key) => {
-            // vēl viens veids kā piekļūt objekta īpašībām ir sintaktiski līdzīgs masīva veidam
-            // tomēr šeit norādām nevis indeksu, bet gan īpašības nosaukumu - key
-            // parsedContacts[key] piekļūstam objekta īpašībai pēc tās nosaukuma
+Object.keys(parsedContacts).forEach((key) => {
             let itemAsString = `<li>${key}: ${parsedContacts[key]}</li>`;
             contactsToDisplay.push(itemAsString);
         })
@@ -71,64 +57,3 @@ window.onload = () => {
         localStorage.setItem("contacts", contactsAsJSON);
     }
 }
-
-// sesion storage
-// window.onload = () => {
-//     let userName = sessionStorage.getItem("userName");
-//     let userSpeciality = sessionStorage.getItem("speciality");
-//     let userSkills = sessionStorage.getItem("skills");
-//     let userContacts = sessionStorage.getItem("contacts");
-
-//     let userNameOutput = document.getElementById("username");
-//     let specialityOutput = document.getElementById("speciality");
-//     let skillsOutput = document.getElementById("skills");
-//     let contactsOutput = document.getElementById("contacts");
-
-//     if(userName) {
-//         userNameOutput.innerHTML = `Username: ${userName}`;
-//     } else {
-//         sessionStorage.setItem("userName", "Student123");
-//     }
-
-//     if(userSpeciality) {
-//         specialityOutput.innerHTML = `Speciality: ${userSpeciality}`;
-//     } else {
-//         sessionStorage.setItem("speciality", "Frontend");
-//     }
-
-//     if(userSkills) {
-//          let skillsToDisplay = [];
-//          let parsedSkills = JSON.parse(userSkills);
-//          console.log(parsedSkills)
-
-//          parsedSkills.forEach((item) => {
-//             let itemAsString = `<li>${item}</li>`;
-//             skillsToDisplay.push(itemAsString);
-//         })
-
-//         skillsOutput.innerHTML = skillsToDisplay.join("");
-//     } else {
-//         let frontendSkills = ["JavaScript", "React", "CSS"];
-//         let skillsAsJSON = JSON.stringify(frontendSkills);
-//         sessionStorage.setItem("skills", skillsAsJSON);
-//     }
-
-//     if(userContacts) {
-//         let contactsToDisplay = [];
-//         let parsedContacts = JSON.parse(userContacts);
-
-//         Object.keys(parsedContacts).forEach((key) => {
-//             let itemAsString = `<li>${key}: ${parsedContacts[key]}</li>`;
-//             contactsToDisplay.push(itemAsString);
-//         })
-
-//         contactsOutput.innerHTML = contactsToDisplay.join("");
-//     } else {
-//         let contacts = {
-//             email: "student123@gmail.com",
-//             tel: "20034593"
-//         }
-//         let contactsAsJSON = JSON.stringify(contacts);
-//         sessionStorage.setItem("contacts", contactsAsJSON);
-//     }
-// }
