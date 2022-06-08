7a03f4f065cc6cff4b74a6c5eeba98bc411689bc
Keitija Janevica <k.janevica@gmail.com>
2022-06-08 14:40:48 +0300

    06-keitija-janevica

--- majasdarbi/06-majasdarbs/KeitijaJanevica/script.js
+++ majasdarbi/06-majasdarbs/KeitijaJanevica/script.js
@@ -1,15 +1,34 @@
-let characters = ['a','b','c','d','e','f','g','h','i','j','k,','l','m','n','o','p','q','r','s','t','u','v','w','x,','y','z'];
-
-let arrayId = prompt("Ievadiet skaitli no 0 lidz 26", );
-
+const characters = ['a','b','c','d','e','f','g','h','i','j','k,','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let arrayId = 0;
 let output = document.getElementById('main');
-output.innerText = characters[arrayId];
+
+let getValue = () => {
+    arrayId = Number(prompt("Ievadiet skaitli no 1 lidz 26"));
+    arrayId -= 1;
+    if (isNaN(arrayId)) {
+        alert("Ievadītā vērtība nav skaitlis!");
+        getValue();
+    } else if (arrayId > 0 && arrayId <= characters.length +1) {
+        printValues();
+    } else {
+        alert("Ievadītā vērtība nav intervālā no 1-26!");
+        getValue();
+    };
+}
+
+let printValues = () => {
+    output.innerText = characters[arrayId];
+};
+
+getValue();
 
 document.addEventListener("keypress", (eventObject) => {
     if (eventObject.key === characters[arrayId]) {
         console.log('yes');
+        alert(`Bingo!`)
     } else {
         console.log('no');
+        alert(`Pamēģini vēl reiz!`)
     }
 })
 
