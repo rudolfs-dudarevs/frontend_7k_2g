8489192b4c6173d8a8ff7075fe0467935ddae2d8
Sandris Turlajs <sandris.turlajs1@gmail.com>
2022-06-17 18:51:54 +0300

    05-Sandris-Turlajs

---
+++ majasdarbi/05-majasdarbs/05-Sandris-Turlajs/js/cv-as-object.js
@@ -0,0 +1,53 @@
+const VardsUzvards = {
+    fullName: "Sandris Turlajs",
+    profession: "Sales manager",
+    contactInfo: {
+        email: "sandris.turlajs1@gmail.com",
+    },
+    workExperience: [
+        {
+            companyNamePosition: '"BLT9" – Sales manager',
+            fromTill: "2020–current",
+            positionInfo: [
+                "Customer service and consulting",
+                "Accepting and delivering orders",
+                "Order processing in computer programs",
+                "Edit nomenclature and prices",
+                "Communicate with suppliers",
+                "Work with a forklift"
+            ]
+        },
+    ],
+    education: [
+        {
+            universityProgramm: "University of Latvia, Bachelor of Science in Mathematics",
+            fromTill: "2015–2019"
+        },
+        {
+            universityProgramm: "Nautreni Secondary School",
+            fromTill: "2003-2015"
+        }
+    ],
+
+    skills: [
+        "Driver's license"
+    ],
+    faq: [
+        {
+            hobbies: [
+                "volleyball",
+                "Beach volleyball",
+                "geocaching",
+                "gaming CS:GO",
+                "Sudoku/ puzzles",
+                "Board games",
+                "Exit rooms"
+            ]
+        }
+    ],
+    languages: [
+        "Latvian (native)",
+        "English (B2)",
+        "Russian (B1)"
+    ],
+}
